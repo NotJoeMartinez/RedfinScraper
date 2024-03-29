df276518a3c6b4631e9cb539738ee603dd372c91
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2024-03-28 19:49:26 -0500

    more cli options

--- redfin_cli/redfin_cli.py
+++ redfin_cli/redfin_cli.py
@@ -1,4 +1,5 @@
 import click
+import datetime
 from .redfin_scraper import RedfinScraper
 
 REDFIN_VERSION = '0.1.0'
@@ -11,12 +12,36 @@ def cli():
 
 
 @cli.command()
-@click.option("--zip-code", "-z", required=True, help="Get houses by zip code")
+@click.option("--zip-code", "-z", required=False, help="Get houses by zip code")
+@click.option("--city-state", "-c", required=False, help="Get houses by city and state")
 @click.option("--data-path", "-d", required=True, help="Path to zip code database")
-def zip(zip_code, data_path):
+@click.option("--sold-by", "-s", required=False, help="1mo','3mo','6mo','1yr','2yr','3yr','5yr' ")
+def export(zip_code, data_path, city_state, sold_by):
+
+    if zip_code is None and city_state is None:
+        print("Please provide either a zip code or a city-state")
+        return
+    
+
     scraper = RedfinScraper()
+
     scraper.setup(data_path,multiprocessing=False)
-    scraper.scrape(city_states=None,zip_codes=[zip_code])
+
+    if sold_by is None:
+        scraper.scrape(zip_codes=[zip_code], city_states=[city_state])
+    else:
+        scraper.scrape(zip_codes=[zip_code], 
+                       city_states=[city_state], 
+                       sold=True,
+                       sale_period=sold_by)
 
     data = scraper.get_data()
-    data.to_csv("data.csv", index=False)
+    # remove SALE TYPE = In accordance with local MLS rules, some MLS listings are not included in the download
+
+    clean_data = data[data['SALE TYPE'] != 'In accordance with local MLS rules, some MLS listings are not included in the download']
+
+    date_str = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+    out_csv = f"{date_str}_data.csv"
+    print(f"Exporting data to {out_csv}")
+    clean_data.to_csv(out_csv, index=False)
